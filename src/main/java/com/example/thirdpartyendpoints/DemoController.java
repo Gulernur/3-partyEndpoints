5f0acd68cfe6f49582726f0b1c5f8b048d5e1a7a
gulernurdonmez <gule0153@stud.kea.dk>
2022-11-08 13:07:05 +0100

    Initial commit

---
+++ src/main/java/com/example/thirdpartyendpoints/DemoController.java
@@ -0,0 +1,19 @@
+package com.example.thirdpartyendpoints;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private final int SLEEP_TIME = 1000*3;
+
+    @GetMapping(value = "/random-string-slow")
+    public String slowEndpoint() throws InterruptedException {
+        Thread.sleep(SLEEP_TIME); //3 sekunder vent med at eksekvere det nedester kode.
+        return RandomStringUtils.randomAlphanumeric(10);
+    }
+
+}
